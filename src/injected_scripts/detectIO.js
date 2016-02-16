93e0644a042e50ac56d98f8e0ca6866e0fcb456b
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-02-16 19:16:08 +0800

    remove unneeded files.

--- src/injected_scripts/detectIO.js
+++ src/injected_scripts/detectIO.js
@@ -2,7 +2,6 @@
 (function(){
   console.log('run');
   if(window.io){
-    //TODO: try short-circuiting the message passing process but directly invoking chrome.devtools.sendMessage
     window.postMessage({type: 'socketiodev', data: {type: 'connect', message:'io-global'}}, '*');
 
   }else{
