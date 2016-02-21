e99862234b8cca79e37d8354e320353e0657251b
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-02-21 14:01:08 +0800

    add polling to detectIOProps.

--- src/injected_scripts/detectIOProps.js
+++ src/injected_scripts/detectIOProps.js
@@ -1,28 +1,32 @@
 (function(){
-
-	console.log('running');
-  var io = window.io;
-  console.log(io.managers);
-  for(manager in io.managers){
-    if(io.managers.hasOwnProperty(manager)) {
-      console.log(manager);
-      window.postMessage({type: 'socketiodev', data: {type: 'manager', message: manager}}, '*');
-      io.managers[manager].engine.on('packetCreate', function(msg){
-        window.postMessage({type: 'socketiodev', data: {type: 'packetCreate', manager: manager, message: msg}}, '*');
-      })
-      io.managers[manager].engine.on('data', function(msg){
-        window.postMessage({type: 'socketiodev', data: {type: 'packetRcv', manager: manager, message: msg}}, '*');
-      });
-      console.log('taking sockets');
-      var sockets = io.managers[manager].nsps;
-      for(skt in sockets){
-        console.log('skt', skt);
-        if(sockets.hasOwnProperty(skt)){
-          console.log('posting');
-          window.postMessage({type: 'socketiodev', data: {type: 'socket', manager: manager, message: skt}}, '*');
+  var loggedManagers = {};
+  var init = function() {
+    console.log('running');
+    var io = window.io;
+    console.log(io.managers);
+    for (manager in io.managers) {
+      if (io.managers.hasOwnProperty(manager) && !loggedManagers[io.managers[manager].engine.id]) {
+        loggedManagers[io.managers[manager].engine.id] = io.managers[manager];
+        console.log(manager);
+        window.postMessage({type: 'socketiodev', data: {type: 'manager', message: manager}}, '*');
+        io.managers[manager].engine.on('packetCreate', function (msg) {
+          window.postMessage({type: 'socketiodev', data: {type: 'packetCreate', manager: manager, message: msg}}, '*');
+        })
+        io.managers[manager].engine.on('data', function (msg) {
+          window.postMessage({type: 'socketiodev', data: {type: 'packetRcv', manager: manager, message: msg}}, '*');
+        });
+        console.log('taking sockets');
+        var sockets = io.managers[manager].nsps;
+        for (skt in sockets) {
+          console.log('skt', skt);
+          if (sockets.hasOwnProperty(skt)) {
+            console.log('posting');
+            window.postMessage({type: 'socketiodev', data: {type: 'socket', manager: manager, message: skt}}, '*');
+          }
         }
       }
     }
-  }
-
+    setTimeout(init, 2000);
+  };
+  init();
 })();
