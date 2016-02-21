1f941df3307062ec4537d3f7ea8df84740ab9e4e
Michael Limantara <mike.bdg@gmail.com>
2016-02-22 01:34:50 +0800

    Display list of sockets when manager is clicked

--- src/panel/scripts/devtool.js
+++ src/panel/scripts/devtool.js
@@ -4,9 +4,15 @@ var managers = {};
 if (window.messenger) {
   messenger.on('manager', function(data) {
     if (managers[data] === undefined) {
-      managers[data] = null;
+      managers[data] = {};
     }
     renderManagers();
+    window.logger.emit('log', 'adding manager');
+  });
+
+  messenger.on('socket', function(data) {
+    addSocketToManager(data.manager, data.message);
+    window.logger.emit('log', 'adding socket ' + data.message + ' to manager ' + data.manager);
   });
 
   document.getElementById('hello').innerText = 'messenger detected';
@@ -22,12 +28,54 @@ if (window.messenger) {
   document.getElementById('hello').innerText ='no messenger';
 }
 
+function addSocketToManager(managerName, socket) {
+  if (managers[managerName]) {
+    if (!managers[managerName][socket]) {
+      managers[managerName][socket] = {};
+    }
+  }
+}
+
+function getSocket(managerName, socketName) {
+  if (managers[manager] === undefined || managers[manager][socketName] === undefined) {
+    return undefined;
+  }
+
+  return managers[managerName][socketName];
+}
+
+function addSentPacketToSocket(managerName, socketName, type, packet) {
+  var socket = getSocket(managerName, socketName);
+  if (!socket) {
+    if (!socket[type]) {
+      socket[type] = [];
+    }
+    socket[type].push(packet);
+  }
+}
+
 function renderManagers() {
-  $("#manager").html();
+  $("#manager").html('');
   $("#manager").append("List of managers:");
   $("#manager").append("<ul>");
   for (var manager in managers) {
-    $("#manager").append("<li id='"+manager+"'>" + manager + "</li>");
+    $("#manager").append('<li class="manager" id="' + manager + '"">' + manager + '</li>');
   }
   $("#manager").append("</ul>");
+
+  $(".manager").on("click", function() {
+    var managerName = $(this).attr('id');
+    renderSockets(managerName);
+  })
+}
+
+function renderSockets(managerName) {
+  $("#socket").html('');
+  $("#socket").append("List of sockets of " + managerName + ":");
+  $("#socket").append("<ul>");
+  var manager = managers[managerName];
+  for (var socket in manager) {
+    $("#socket").append("<li>" + socket + "</li>");
+  }
+  $("#socket").append("</ul>");
 }
