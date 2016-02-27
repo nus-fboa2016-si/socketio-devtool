5983ab767dc2955ad77e11e6a8fd8bf057024ee9
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-02-27 19:16:44 +0800

    update repo names

--- src/devtoolsBackground.js
+++ src/devtoolsBackground.js
@@ -3,7 +3,7 @@ var panels = chrome && chrome.devtools && chrome.devtools.panels;
 
   // Angular panel
   var panel = panels.create(
-    "TEST2",
+    "Socket.io",
     "icon.png",
     "panel/panel.html"
   );
