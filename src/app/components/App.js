d4d755b225e92faad6c2112b2eae7c223e03468b
Michael Limantara <mike.bdg@gmail.com>
2016-03-14 03:12:26 +0800

    Display searchable packet list and packet content box component in the root view

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,8 +1,20 @@
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import {addPacket, addSocket} from '../actions/updateActions';
+import CSSModules from 'react-css-modules';
+import SearchablePacketListBox from './SearchablePacketListBox';
+import PacketContentBox from './PacketContentBox';
+import styles from '../styles/packets_panel.scss';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		document.body.style.width = '100%';
+		document.body.style.height = '100%';
+		document.body.style.margin = '0px';
+		document.body.style.padding = '0';
+	}
+
 	componentDidMount(){
 		const {addSocket, addPacket} = this.props;
 		let messenger = window['__SOCKETIO-DEVTOOL-MESSENGER__'];
@@ -21,9 +33,11 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div>
+			<div styleName='main-content'>
+				<SearchablePacketListBox />
+				<PacketContentBox />
 			</div>
-		)
+		);
 	}
 }
 
@@ -36,4 +50,4 @@ const mapStateToProps = function(state){
 	return {};
 };
 
-export default connect(mapStateToProps, {addSocket, addPacket})(App);
+export default connect(mapStateToProps, {addSocket, addPacket})(CSSModules(App,styles));
