04e7c6401086914243c5dcdee6f4011139831124
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-03-14 18:31:41 +0800

    update styling.

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
 				<Header/>
 				<div className="body">
 					<SearchablePacketListBox />
-					< PacketContentBox />
+					<PacketContentBox />
 				</div>
 			</div>
 		);
